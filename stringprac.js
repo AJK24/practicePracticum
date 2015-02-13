162a2c55293f16648080a483ade261d1cc02562d
Andrew <akv9509@saintjoe.edu>
2015-02-13 12:50:46 -0500

    Added text string changes and accessing

---
+++ stringprac.js
@@ -0,0 +1,17 @@
+//andrwe klump
+//type in a string at least 10 mixed case
+//print out the 4th 7th and 10th characters
+//turn it all pper case or all lower case
+//
+//
+//
+
+
+var tester = "The quick Brown fox jumped";
+
+
+console.log('4 and 7 and 10 ' + tester[4] + ' ' + tester[7] + ' ' +  tester[10]);
+
+tester = tester.toUpperCase();
+
+console.log(tester);
